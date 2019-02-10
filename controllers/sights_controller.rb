39d327339ecc4795496718a760b5841c6ca8aebc
Pauline Rudge <paulinerudge@me.com>
2019-02-10 18:01:34 +0000

    now that I am using the sights button I realise it needs a comments bar

--- controllers/sights_controller.rb
+++ controllers/sights_controller.rb
@@ -5,6 +5,16 @@ require_relative('../models/city.rb')
 require_relative('../models/country.rb')
 also_reload('../models/*')
 
+get '/sights/new/:id' do #form for new sight
+  @city = City.find(params['id'])
+  erb(:"sights/new")
+end
+
+post '/sights' do #add a new sight (for a city in a country
+  Sight.new(params).save
+  redirect to "/sights/#{params['city_id']}"
+end
+
 get '/sights/:id' do #display sights for a city/country
   @city = City.find(params['id'])
   id = @city.country_id
